25c20e96ce2555355ed349af79e520203179428b
Custodiojr <cmassingue8@gmail.com>
2023-04-27 16:13:37 +0200

    Atualização do  método GET para ler parâmetros e adição dos métodos,  PUT e DELETE

--- projecto_API/api/views.py
+++ projecto_API/api/views.py
@@ -11,26 +11,55 @@ import json
 
 class CompanyView(View):
     @method_decorator(csrf_exempt)
-    def dispatch(self, request ,  *args, **kwargs):
+    def dispatch(self, request,  *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
-    
-    def get(self, request):
-        companies = list(Company.objects.values())
-        if len(companies) > 0:
-            data = {'message': "Success", 'companies': companies}
+
+    def get(self, request, id=0):
+        if (id > 0):
+            companies = list(Company.objects.filter(id=id).values())
+            if len(companies) > 0:
+                company = companies[0]
+                data = {'message': "Success", 'company': company}
+            else:
+                data = {'message': "Company not found..."}
+            return JsonResponse(data)
         else:
-            data = {'message': "Companies not found ..."}
-        return JsonResponse(data)
+            companies = list(Company.objects.values())
+            if len(companies) > 0:
+                data = {'message': "Success", 'companies': companies}
+            else:
+                data = {'message': "Companies not found ..."}
+            return JsonResponse(data)
 
     def post(self, request):
-       
+
         jd = json.loads(request.body)
-        Company.objects.create(name=jd['name'], website=jd['website'], foundation=jd['foundation'])
+        Company.objects.create(
+            name=jd['name'], website=jd['website'], foundation=jd['foundation'])
         data = {'message': "Success"}
         return JsonResponse(data)
 
-    def put(self, request):
-        pass
+    def put(self, request, id):
+        # Pega o corpo do json, onde tem a informacao para atualizar
+        jd = json.loads(request.body)
+        # Traz somente os objectos que quero atualizar
+        companies = list(Company.objects.filter(id=id).values())
+        if len(companies) > 0:
+            company = Company.objects.get(id=id)
+            company.name = jd['name']
+            company.website = jd['website']
+            company.foundation = jd['foundation']
+            company.save()
+            data = {'message': "Success"}
+        else:
+            data = {'message': "Company not found ..."}
+        return JsonResponse(data)
 
-    def delete(self, request):
-        pass
+    def delete(self, request,id):
+        companies = list(Company.objects.filter(id=id).values())
+        if len(companies) > 0:
+            Company.objects.filter(id=id).delete()
+            data = {'message': "Success"}
+        else:
+            data = {'message': "Company not found ..."}
+        return JsonResponse(data)
